0850cefdbb8593e5f37354c7f8fbac0cb533b519
Bram de Smidt <bwdesmidt@gmail.com>
2023-06-21 22:58:57 +0200

    Add missing config

--- karoosync.py
+++ karoosync.py
@@ -13,6 +13,7 @@ def write_configfile(filename):
 INTERVALS_ICU_ID = i00000
 INTERVALS_ICU_APIKEY = 00000000000000000000
 # Date or date range of intervals you want to upload to your Karoo device
+TODAY_ONLY=False # If true the other date fields will be ignored
 WORKOUT_OLDEST_DATE = 2022-01-01
 WORKOUT_NEWEST_DATE = 2022-12-31
 
